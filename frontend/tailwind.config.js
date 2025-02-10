40a6518cf90b8927ed4a6f0395a4c0079b856948
yoonjookim <yjblossom@gmail.com>
2025-02-10 15:23:19 -0500

    Style TopTherapist component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
     extend: {
       colors:{
         'primary':"#FDE68A"
+      },
+      gridTemplateColumns:{
+        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
