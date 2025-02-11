3bb14705bc65c0d301eb9e8370738f9697dfcf9e
yoonjookim <yjblossom@gmail.com>
2025-02-10 23:00:36 -0500

    Style Banner Component

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Banner = () => {
+
+    
   return (
-    <div>
+    <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
       {/* -------Left Side-------- */}
-        <div>
-            <img src={assets.appointment_img} alt="" />
+        <div className='hidden md:block md:w-1/2 lg:w-[370px] relative'>
+            <img className='w-full absolute bottom-0 right-0 max-w-md' src={assets.appointment_img} alt="" />
         </div>
 
       {/* -------Right Side-------- */}
-        <div>
-            <div>
-                <p>Book Appointment</p>
+        <div className='flex-1 py-8 sm:py-10 md:py-16 lg:py-24 lg:pl-5'>
+            <div className='text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-blue-300'>
+                <p className='mt-4'>Book Appointment</p>
                 <p>With 50+ Trusted Therapists</p>
             </div>
-            <button>Create Account</button>
+            <button  className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Account</button>
         </div>
 
     </div>
