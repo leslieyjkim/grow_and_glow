8ab3f212ac44786337978d128164170dd0cfc3f7
yoonjookim <yjblossom@gmail.com>
2025-02-10 23:02:56 -0500

    Function button navigate to login page

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const Banner = () => {
 
-    
+    const navigate = useNavigate()
+
   return (
     <div className='flex bg-primary rounded-lg px-6 sm:px-10 md:px-14 lg:px-12 my-20 md:mx-10'>
       {/* -------Left Side-------- */}
@@ -17,7 +19,7 @@ const Banner = () => {
                 <p className='mt-4'>Book Appointment</p>
                 <p>With 50+ Trusted Therapists</p>
             </div>
-            <button  className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Account</button>
+            <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create Account</button>
         </div>
 
     </div>
