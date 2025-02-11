95f34b2cb84e2f68722fbee0585c85e64fc8ca82
yoonjookim <yjblossom@gmail.com>
2025-02-11 17:53:04 -0500

    Feature Appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import {AppContext} from '../context/AppContext'
 
 const Appointment = () => {
+
+  const {therapistId} = useParams()
+  const {therapists} = useContext(AppContext)
+
+  const [therapistInfo, setTherapistInfo] = useState(null)
+
+  const fetchTherapistInfo = async () => {
+    const therapistInfo = therapists.find(therapist => therapist._id === therapistId)
+    setTherapistInfo(therapistInfo)
+    console.log(therapistInfo);
+  }
+
+  useEffect(()=>{
+    fetchTherapistInfo()
+  },[therapists, therapistId])
+
   return (
     <div>
       
