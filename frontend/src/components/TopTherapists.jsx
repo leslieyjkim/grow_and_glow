e06891b2fb0105cad3a61bd401ecce61d07503fa
yoonjookim <yjblossom@gmail.com>
2025-02-10 22:41:36 -0500

    Function 'more' button to therapists page

--- frontend/src/components/TopTherapists.jsx
+++ frontend/src/components/TopTherapists.jsx
@@ -14,7 +14,7 @@ const TopTherapists = () => {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {therapists.slice(0,10).map((item,index)=>(
             <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
-                <img className='bg-blue-200' src={item.image} alt=""/>
+                <img className='bg-gray-50' src={item.image} alt=""/>
                 <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
                         <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
@@ -25,7 +25,7 @@ const TopTherapists = () => {
             </div>
         ))}
       </div>
-      <button className='bg-primary text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+      <button onClick={()=>{navigate('/therapists'); scrollTo(0,0)}} className='bg-primary text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
