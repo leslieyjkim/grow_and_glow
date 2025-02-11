9e7bf4bb979062528ab4139f967ac75411f5673e
yoonjookim <yjblossom@gmail.com>
2025-02-10 22:22:28 -0500

    Function link to the therapist's page

--- frontend/src/components/TopTherapists.jsx
+++ frontend/src/components/TopTherapists.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { therapists } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const TopTherapists = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium'>Top Therapists to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted therapists.</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {therapists.slice(0,10).map((item,index)=>(
-            <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+            <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img className='bg-white' src={item.image} alt=""/>
                 <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
