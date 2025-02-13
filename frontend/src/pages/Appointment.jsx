5d4e6b83a3ca2f11bfb4f7df558c03a2065f8442
yoonjookim <yjblossom@gmail.com>
2025-02-13 12:53:17 -0500

    Feature Appointment page (1)

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,29 +1,46 @@
-import React, { useContext, useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
-import {AppContext} from '../context/AppContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
 
 const Appointment = () => {
+  const { therapistId } = useParams();
+  const { therapists } = useContext(AppContext);
 
-  const {therapistId} = useParams()
-  const {therapists} = useContext(AppContext)
+  const [therapistInfo, setTherapistInfo] = useState(null);
 
-  const [therapistInfo, setTherapistInfo] = useState(null)
+  const fetchTherapistInfo = () => {
+    if (therapists && therapists.length > 0) {
+      const info = therapists.find(therapist => therapist._id === therapistId);
+      setTherapistInfo(info || null);  // Explicitly set to null if not found
+    }
+  };
 
-  const fetchTherapistInfo = async () => {
-    const therapistInfo = therapists.find(therapist => therapist._id === therapistId)
-    setTherapistInfo(therapistInfo)
-    console.log(therapistInfo);
-  }
+  useEffect(() => {
+    if (therapists && therapists.length > 0 && therapistId) {
+      fetchTherapistInfo();
+    }
+  }, [therapists, therapistId]);
 
-  useEffect(()=>{
-    fetchTherapistInfo()
-  },[therapists, therapistId])
-
-  return (
+  return therapistInfo && (
     <div>
-      
+      {/* -----------Therapist Details----------- */}
+      <div>
+        <div>
+          {therapistInfo ? (
+            <div>
+              {therapistInfo.image ? (
+                <img src={therapistInfo.image} />
+              ) : (
+                <p>No image available for this therapist.</p>
+              )}
+            </div>
+          ) : (
+            <p>Loading therapist details...</p>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Appointment
+export default Appointment;
