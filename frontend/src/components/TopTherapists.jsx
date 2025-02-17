9ef02fa6c9157992f1ba91755e2dc3736d9eb121
yoonjookim <yjblossom@gmail.com>
2025-02-17 17:13:53 -0500

    Revise TopTherapists page to connect therapist page

--- frontend/src/components/TopTherapists.jsx
+++ frontend/src/components/TopTherapists.jsx
@@ -12,8 +12,8 @@ const TopTherapists = () => {
       <h1 className='text-3xl font-medium'>Top Therapists to Book</h1>
       <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted therapists.</p>
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {therapists.slice(0,10).map((item,index)=>(
-            <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+        {therapists.slice(0,15).map((item,index)=>(
+            <div onClick={()=>{navigate(`/appointment/${item._id}`); scrollTo(0,0)}} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img className='bg-gray-50' src={item.image} alt=""/>
                 <div className='p-4'>
                     <div className='flex items-center gap-2 text-sm text-center text-green-500'>
