6c2fb75d8d422d7a1762e775bc18ce64bacd45dd
yoonjookim <yjblossom@gmail.com>
2025-02-18 13:15:36 -0500

    Feature Login page(2)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,8 +14,24 @@ const Login = () => {
   }
 
   return (
-    <form>
-
+    <form className='min-h-[80vh] flex items-center'>
+      <div>
+        <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
+        <p>Please {state === 'Sign Up' ? "Sign up" : "Log in"} to book appointment</p>
+        <div>
+          <p>Full Name</p>
+          <input type="text" onChange={(e)=>setName(e.target.name)} value={name}/>
+        </div>
+        <div>
+          <p>Email</p>
+          <input type="email" onChange={(e)=>setEmail(e.target.email)} value={email}/>
+        </div>
+        <div>
+          <p>Password</p>
+          <input type="password" onChange={(e)=>setPassword(e.target.password)} value={password}/>
+        </div>
+        <button>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+      </div>
     </form>
   )
 }
