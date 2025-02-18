1f0ca23933cd0a6f2cc7e96b31d2422bf1c0b03e
yoonjookim <yjblossom@gmail.com>
2025-02-18 13:05:46 -0500

    Feature Login page(1)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state, setState] = useState('Sign Up')
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  //when we submit, it won't reload page
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form>
+
+    </form>
   )
 }
 
