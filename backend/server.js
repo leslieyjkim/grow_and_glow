6f739b43769eef10390531985e07c088045aecb0
yoonjookim <yjblossom@gmail.com>
2025-02-19 17:09:49 -0500

    Initial Setup Express App(2)

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+
+
+// middlewares : these will allow to connect with backend
+app.use(express.json())
+app.use(cors())
+
+// api endpoint
+app.get('/', (req, res)=>{
+    res.send('API WORKING')
+})
+
+app.listen(port, () => console.log("Server Started", port))
