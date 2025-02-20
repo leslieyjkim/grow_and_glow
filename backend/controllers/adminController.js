ff6cd81060a7dd05d2e8a396f105b7c98b11546b
yoonjookim <yjblossom@gmail.com>
2025-02-20 15:22:43 -0500

    Feature adminController and multer(middleware)

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,17 @@
+
+
+
+
+// API for adding therapist
+const addTherapist = async (req,res) => {
+
+    try {
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+
+
+    } catch (error) {
+
+    }
+}
+
+export {addTherapist}
